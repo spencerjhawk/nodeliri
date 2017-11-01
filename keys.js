4c0f280c79833d96081f9dde58f9469ccbebe010
Spencer Hawk <spencerjhawk@gmail.com>
2017-11-01 16:57:07 -0500

    bug fixes

--- keys.js
+++ keys.js
@@ -3,7 +3,14 @@ console.log('this is loaded');
 
 var twitterKeys = {
   consumer_key: '<3CqqsC4uarW7Pabo4tcogyDlf>',
-  consumer_secret: '<input here>',
+  consumer_secret: '<h3L6JrNyTHtuug3bs7YP1GOads9Fo1EJQmxjs78pu0tmQ89Dat>',
+  access_token_key: '<input here>',
+  access_token_secret: '<input here>',
+}
+
+var spotifyKeys = {
+  consumer_key: '<d8ee98e1d1cb4d2b8908958a9b78750b>',
+  consumer_secret: '<a295ef9d3a8d43ab8c028b514d8c73ba>',
   access_token_key: '<input here>',
   access_token_secret: '<input here>',
 }
--- liri.js
+++ liri.js
@@ -5,6 +5,21 @@ var twitter = require('twitter');
 var spotify = require('spotify');
 var request = require('request');
 
+
+//Logs info to txt file
+var writeToLog = function(data) {
+  fs.appendFile("log.txt", '\r\n\r\n');
+
+  fs.appendFile("log.txt", JSON.stringify(data), function(err) {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log("log.txt was updated!");
+  });
+}
+
+
 //Twitter function
 var getTweets = function() {
   var client = new twitter(dataKeys.twitterKeys);
@@ -26,3 +41,37 @@ var getTweets = function() {
     }
   });
 };
+
+
+//Omdb function
+var getMeMovie = function(movieName) {
+
+  if (movieName === undefined) {
+    movieName = 'Big';
+  }
+
+  var urlHit = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&r=json";
+
+  request(urlHit, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = [];
+      var jsonData = JSON.parse(body);
+
+      data.push({
+      'Title: ' : jsonData.Title,
+      'Year: ' : jsonData.Year,
+      'Rated: ' : jsonData.Rated,
+      'IMDB Rating: ' : jsonData.imdbRating,
+      'Country: ' : jsonData.Country,
+      'Language: ' : jsonData.Language,
+      'Plot: ' : jsonData.Plot,
+      'Actors: ' : jsonData.Actors,
+      'Rotten Tomatoes Rating: ' : jsonData.tomatoRating,
+      'Rotton Tomatoes URL: ' : jsonData.tomatoURL,
+  });
+      console.log(data);
+      writeToLog(data);
+}
+  });
+
+}
