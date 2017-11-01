30eb1abaeb92a8f4be7558a59ac8ea718831cf9b
Spencer Hawk <spencerjhawk@gmail.com>
2017-11-01 18:09:09 -0500

    more fixes that dont quite work

--- keys.js
+++ keys.js
@@ -11,8 +11,6 @@ var twitterKeys = {
 var spotifyKeys = {
   consumer_key: 'd8ee98e1d1cb4d2b8908958a9b78750b',
   consumer_secret: 'a295ef9d3a8d43ab8c028b514d8c73ba',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
 }
 
 module.exports = twitterKeys;
